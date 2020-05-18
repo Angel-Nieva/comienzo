55993dafd994b232c1a0d1edfc02381dff36538c
Angel Nieva <angel.nieva@usach.cl>
2020-05-18 02:06:33 -0400

    Agregado selectores de repositorios e ideando TDA repositorio

--- TDA_funciones.rkt
+++ TDA_funciones.rkt
@@ -1,21 +1,36 @@
 #lang racket
 ;TDA Comandos: nos permiten trabajar entre las zonas de trabajo, por lo que seran puramente funciones
 
-;La funcion git es un tipo de funcion de pertenencia, tiene como parametro una funcion y en cao de que el parametro no corresponda a una funcion retorna #f
+
+
+
+;La funcion git es un tipo de funcion de pertenencia, tiene como parametro una funcion y en caso de que el parametro no corresponda a una funcion retorna #f
 (define git (lambda (funcion)
-              (if (= (funcion? (funcion)) #t) 
-                  (lambda (parametros)(funcion parametros))
-                  #f
+              (lambda (parametros)
+                    (if (= (funcion? (funcion)) #t) 
+                         (lambda (parametros)(funcion parametros))
+                          "Funcion invalida"
+                    )      
               )    
-            )
+       )
 )
 
 ;Pregunta si un parametro es una funcion, retorna true si corresponde a alguna de las funciones o false si no
 (define funcion? (lambda(funcion)
-                   (if (= #t (or (pull? funcion)(add? funcion)(commit? funcion)(push? funcion)) )
+                   (if (or (pull? funcion)(add? funcion)(commit? funcion)(push? funcion)) 
                        #t
                        #f
-                    )
-                  )
+                 )
+         )
 )
 
+
+;Git: Funcion que recibe una lista con los nombres de los archivos en workspace y los lleva a index, si la lista esta vacia lleva todos los archivos en index
+
+(define add (lambda (lista)
+              (if (= (null? lista) #t)
+                  (add-all lista)
+                  ()
+              )     
+        )
+)
--- TDA_repositorios.rkt
+++ TDA_repositorios.rkt
@@ -1,11 +1,53 @@
 #lang racket
 ;TDA Repositorios:
-;Esta corresponden a las 4 zonas de trabajo: Workspace,Index,Local Repository y Remote Repository donde basicamente cada zona es una lista de listas.
+;Esta compuesta por las 4 zonas de trabajo: Workspace,Index,Local Repository y Remote Repository donde basicamente cada zona es una lista de listas.
+;Tambien contiene el nombre del repositorio, que correspondera a la ramificacion actual de los commits
+;Ej:  '(master,workspace,index,local_rep,remote_rep)
 
-(define constructor_repositorio (lambda (workspace index local_repository remote_repository)(list "repositorio 1" "Fecha" workspace index local_repository remote_repository)))
 
-(define constructor_workspace(lambda (codigo)(list codigo)))
-(define constructor_index(lambda (codigo)(list codigo)))
-(define constructor_local_repository(lambda (commit)(list commit)))
-(define constructor_remote_repository(lambda (commit)(list commit)))
+
+;(define constructor_repositorio (lambda ("master" workspace index local_repository remote_repository)(list "master" workspace index local_repository remote_repository)))
+
+;TDA
+
+;----------------------------------------------------------------CONSTRUCTORES------------------------------------------------------------------------------------
+;Funciones que in
+(define cons_workspace (lambda (workspace) (if (workspace? workspace) workspace (display "Datos no corresponden a un workspace\n")  )))
+(define cons_index (lambda (index) (if (index? index) index (display "Datos no corresponden a un index\n")  )))
+
+;----------------------------------------------------------------PERTENENCIA---------------------------------------------------------------------------;
+
+;Funcion que verifica recursivamente si una lista esta compuesta unicamente por string o esta vacia
+(define lista_string? (lambda (lista)
+               (if (null? lista)  ;Si el primer elemento de la lista es null retorna #t
+                   #t
+                   (and (list? lista) (string? (car lista)) (lista_string? (cdr lista)))
+               ) 
+        )
+)
+;Funciones que verifican si un dato ingresado corresponde a un workspace o index
+(define workspace? (lambda (lista)(lista_string? lista)))
+(define index? (lambda (lista)(lista_string?)))
+
+;----------------------------------------------------------------SELECTORES-------------------------------------------------------------------------;
+(define get_ramificacion (lambda (zonas)(list-ref zonas 0)))
+(define get_workspace (lambda (zonas)(cadr (list-ref zonas 1))))
+(define get_index (lambda (zonas)(cadr (list-ref zonas 2))))
+(define get_local_rep (lambda (zonas)(cadr (list-ref zonas 3))))
+(define get_remote_rep (lambda (zonas)(cadr (list-ref zonas 4))))
+
+
+
+
+
+
+
+
+
+
+
+
+;(define constructor_local_repository(lambda (commit)(list commit)))
+;(define constructor_remote_repository(lambda (commit)(list commit)))
+
                               
