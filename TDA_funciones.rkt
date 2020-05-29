0f14546f44e5880e4377c6fd38c24c846f1bb9c4
Angel Nieva <angel.nieva@usach.cl>
2020-05-29 18:18:17 -0400

    Termino funcion push. ¿TERMINADA?

--- TDA_funciones.rkt
+++ TDA_funciones.rkt
@@ -6,15 +6,13 @@
 
 
 
-;-----------------------------------------------------------------------GIT----------------------------------------------------------------------------------;
+;-----------------------------------------------------------------------GIT---------------------------------------------------------------------------------------;
 ;Funcion currificada que recive una funcion(comando) con los parametros propios de esta funcion
 ;Entrada: funcion que representa un comando de git, parametro de entrada del comando, repositorio
 ;Salida: comando con los parametros ingresados
 (define git (lambda (funcion)
               (lambda (parametros)
-                  (lambda (zonas)
-                      ((funcion parametros)zonas)
-                   ) 
+                (funcion parametros)    
               )    
        )
 )
@@ -55,31 +53,38 @@
        )
 )
 ;--------------------------------------------------------------------COMMIT-------------------------------------------------------------------------------------;
+;Funcion que retorna el ultimo commit en el repositorio local
+;Entrada: repositorio local
+;Salida: commit
 (define get-last-commit (lambda (local-rep)(if (null? (cdr local-rep)) (car local-rep) (get-last-commit(cdr local-rep)))))
 
+;Funcion que crea un commit con un mensaje descriptivo, los archivos guardados en index y el identificador del ultimo commit guardado en el repositorio local
+;Entrada: mensaje descriptivo y repositorio con las zonas de trabajo
+;Salida: Si el repositorio local esta vacio crea el primer commit (Id anterior = 0), si no retorna un nuevo commit con el id del commit anterior
 (define crear_commit (lambda (mensaje zonas)
                   (if (null? (rep_get_local_rep zonas)) ;Si el repositorio local esta vacio
-                      (cons_commit "Angel" mensaje (rep_get_index zonas) 0) ;Se crea un commit en donde el commit anterior es 0(commit inicial)
+                      (cons_commit "Angel" mensaje (rep_get_index zonas) 0) ;Se crea el commit inicial (id anterior = 0)
                       (cons_commit "Angel" mensaje (rep_get_index zonas) (com_get_id_anterior (get-last-commit (rep_get_local_rep zonas))))
                   )
          )              
 )             
 
+;Funcion que crea un repositorio con un nuevo commit agregado al repositorio local y el index vacio
+;Entrada: mensaje descriptivo y repositorio con las zonas de trabajo
+;Salida: nuevo repositorio con index vacio y cambios guardados en repositorio local
 (define commit-no-curry (lambda (mensaje zonas)
         (rep_set_index (rep_set_local_rep zonas (append (rep_get_local_rep zonas) (list (crear_commit mensaje zonas)) )) '( ) ) ;Se vacia el index 
     )
 ) 
 
-
-
-  
-;Funcion que encapsula la funcion anterior y pregunta si los datos ingresados son correctos
-;Entrada: mensaje(string), repositorio
-;Salida: si los archivos son correctos entrega un nuevo repositorio con un commit ingresados en el repositorio local, sino se despliega un mensaje
+;Funcion que crea un repositorio con un registro de la funcion "commit"
+;Entrada: mensaje descriptivo, repositorio (currificada)
+;Salida: si los datos de entrada son correctos pregunta si el index esta vacio. Si se encuentra vacio retorna zonas sin cambios, sino retorna una nueva version del repositorio.
+;        si los datos de entrada no son correctos retorna un mensaje
 (define commit (lambda (mensaje)
                   (lambda (zonas)
                       (if (and (string? mensaje)(repository? zonas))
-                            (if (null? (rep_get_index zonas)) ;Si el index esta vacio se retorna la zona como esta
+                            (if (null? (rep_get_index zonas)) ;Si el index esta vacio se retorna las zonas como estaba
                                 zonas
                                (rep_set_funciones (commit-no-curry mensaje zonas) (append (rep_get_funciones zonas) '("commit")) );Se deja registro de la funcion "commit"
                             )
@@ -88,9 +93,18 @@
               )
       ) 
 )
+;--------------------------------------------------------------------PUSH----------------------------------------------------------------------------------------;
+;Funcion que envia todos los commits en el repositorio local al repositorio remoto
+;Entrada: zonas
+;Salida: zonas con los cambios guardados y un registro de la funcion "push"
+(define push (lambda (zonas)(rep_set_funciones (rep_set_remote_rep zonas (rep_get_local_rep zonas)) '("push"))))
+;--------------------------------------------------------------------PULL----------------------------------------------------------------------------------------;
+
+
+
 
 
 ;(rep_set_index zonas archivos)
 ;(((git add)'("asd" "asd"))repo1)
-(define commit1 (cons_commit "angel" "" '() 0))
+
 (define repo1 (list "master" '("asd" "str1" "str2") '("str1") '() '() '("add")))
